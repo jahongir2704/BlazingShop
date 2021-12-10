bb2df6bc2aba06fa0e303b8ca0c5d05a9d014998
Jahongir Alisheriv <94122337+jahongir2704@users.noreply.github.com>
2021-12-10 21:29:22 +0500

    ....

--- BlazingShop/Client/Program.cs
+++ BlazingShop/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazingShop.Client;
+using BlazingShop.Client.Services.CategoryService;
 using BlazingShop.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,4 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 await builder.Build().RunAsync();
---
+++ BlazingShop/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using BlazingShop.Shared;
+
+namespace BlazingShop.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public void LoadCategories()
+        {
+            Categories = new List<Category>
+            {
+                new Category{Id = 1, Name = "Video Games", Url = "video-games", Icon = "aperture"},
+                new Category{Id = 2, Name ="Electronics",  Url = "electronics", Icon = "camera-slr"},
+                new Category{Id = 3, Name ="Movies",       Url = "movies",      Icon = "book"}
+            };
+        }
+    }
+}
